356277a2d9e297c87c102346fc9ffdf61d8e6750
zankoav <zankoav@mail.ru>
2018-12-07 22:29:10 +0300

    update

--- templates/content-footer.php
+++ templates/content-footer.php
@@ -2,26 +2,16 @@
             class="navigation__item navigation__item_right" href="#" target="_blank">About us</a></nav>
 <footer class="footer">
     <div class="container">
-        <div class="footer__context"><a class="footer__logo-link" href="#"><img class="footer__logo"
-                                                                                src="/wp-content/themes/69px/src/icons/logo.ced516.svg"></a>
+        <div class="footer__context"><a class="logo " href="/"><img class="logo__icon" src="/wp-content/themes/69px/src/icons/logo.ced516.svg"></a>
             <div class="footer__copy-right">&copy; <span class="footer__copy-right-number">69</span> - 2018
             </div>
-            <ul class="footer__socials">
-                <li class="footer__social"><a class="footer__social-link" href="#"><img class="footer__social-icon"
-                                                                                        src="/wp-content/themes/69px/src/icons/ic-telegram.dd0ffa.svg"></a>
-                </li>
-                <li class="footer__social"><a class="footer__social-link" href="#"><img class="footer__social-icon"
-                                                                                        src="/wp-content/themes/69px/src/icons/ic-telegram.dd0ffa.svg"></a>
-                </li>
-                <li class="footer__social"><a class="footer__social-link" href="#"><img class="footer__social-icon"
-                                                                                        src="/wp-content/themes/69px/src/icons/ic-telegram.dd0ffa.svg"></a>
-                </li>
-                <li class="footer__social"><a class="footer__social-link" href="#"><img class="footer__social-icon"
-                                                                                        src="/wp-content/themes/69px/src/icons/ic-telegram.dd0ffa.svg"></a>
-                </li>
-                <li class="footer__social"><a class="footer__social-link" href="#"><img class="footer__social-icon"
-                                                                                        src="/wp-content/themes/69px/src/icons/ic-telegram.dd0ffa.svg"></a>
-                </li>
+            <ul class="socials undefined ">
+                <li class="socials__item"><a class="socials__link" href="#"><img class="socials__icon" src="/wp-content/themes/69px/src/icons/ic-telegram.dd0ffa.svg"></a></li>
+                <li class="socials__item"><a class="socials__link" href="#"><img class="socials__icon" src="/wp-content/themes/69px/src/icons/ic-instagram.73e216.svg"></a></li>
+                <li class="socials__item"><a class="socials__link" href="#"><img class="socials__icon" src="/wp-content/themes/69px/src/icons/ic-facebook.293ce7.svg"></a></li>
+                <li class="socials__item"><a class="socials__link" href="#"><img class="socials__icon" src="/wp-content/themes/69px/src/icons/linked-in.cbd19f.svg"></a></li>
+                <li class="socials__item"><a class="socials__link" href="#"><img class="socials__icon" src="/wp-content/themes/69px/src/icons/behance.920e86.svg"></a></li>
+                <li class="socials__item"><a class="socials__link" href="#"><img class="socials__icon" src="/wp-content/themes/69px/src/icons/web.b3a778.svg"></a></li>
             </ul>
         </div>
     </div>
