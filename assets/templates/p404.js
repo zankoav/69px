8320fa2ce89ae063faa32bd81ddd0a1f3c990602
zankoav <zankoav@mail.ru>
2018-12-12 20:24:27 +0300

    update

---
+++ assets/templates/p404.js
@@ -0,0 +1,5 @@
+import './../common/_common.scss';
+import './../components/loader/loader';
+import './../components/app/app';
+import './../components/header/header';
+import './../components/footer/footer';
